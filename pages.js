3148f6c7405463fff70aceb2cec096f904276175
devhachimi <34142620+devhachimi@users.noreply.github.com>
2017-12-05 23:13:06 +0100

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,12 +1,12 @@
 var PAGES = {
 
-    '1': ['الله', 'www/001.htm'],
-    '2': ['الجسد', 'www/002.htm'],
-    '3': ['الروح', 'www/003.htm'],
-    '4': ['الباب الرابع في معنى التلبيس والغرور', 'www/004.htm'],
-    '5': ['الباب ا', 'www/005.htm'],
-    '6': ['الباب ', 'www/006.htm'],
-    '7': ['الباب الثا', 'www/007.htm'],
-    '8': ['الباب ', 'www/008.htm']
+    '1': ['الله', 'https://rawgit.com/devhachimi/www/master/001.htm'],
+    '2': ['الجسد', 'https://rawgit.com/devhachimi/www/master/002.htm'],
+    '3': ['الروح', 'https://rawgit.com/devhachimi/www/master/003.htm'],
+    '4': ['الباب الرابع في معنى التلبيس والغرور', 'https://rawgit.com/devhachimi/www/master/004.htm'],
+    '5': ['الباب ا', 'https://rawgit.com/devhachimi/www/master/005.htm'],
+    '6': ['الباب ', 'https://rawgit.com/devhachimi/www/master/006.htm'],
+    '7': ['الباب الثا', 'https://rawgit.com/devhachimi/www/master/007.htm'],
+    '8': ['الباب ', 'https://rawgit.com/devhachimi/www/master/008.htm']
 
 };
